092b6fd68ece4df0298867572987578f542597b9
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-05-30 22:56:43 +0530

    Update sender_main.c

--- Sender_C/sender_main.c
+++ Sender_C/sender_main.c
@@ -1,33 +1,35 @@
 #include "Common_header.h"
+#include "time.h"
 #include "Output_Handler.h"
 #include "sender_main.h"
 
 
 
-int main()
+void main()
 {
+	clock_t timestamp_start;
+	timestamp_start = clock();
 	sender_main();
-	getchar();
-	return 0;
+	while(1)
+	{
+		if(Fc_counter_elapsed(timestamp_start) == TRUE)
+		{
+			timestamp_start = clock();
+			if(sender_main() == PROCESS_TERMINATE)
+			{
+				break;
+			};
+		}
+	}
 }
 
-
-
-
-float string2float(char *str)
-{
-	float number;
-	sprintf(str, "%f", number);
-	return number;
-}
-
-void sender_main(void)
+int sender_main(void)
 {
 	/*Operation steps:
 	1. Collect a set of parameters from CSV file.
 	2. Store it in Local memory/Msg collect
 	3. Send it to the configured user interface*/
-	int Retval;
+	int Retval, doTerminate = FALSE;
 	char *disp_msg; 
 	Sender_st Sender = {CSV_Handler , batt_param_collector , Msg_Handler};
 	/*-----------------------------------------*/
@@ -40,13 +42,45 @@ void sender_main(void)
 	else if(Retval == NOT_OK)
 	{
 		disp_msg  = "CSV file read error" ; 
+		doTerminate = TRUE;
 	}
 	else if(Retval == FILE_END_OK)
 	{
 		disp_msg  = "CSV file end" ; 
+		doTerminate = TRUE;
 	}
 	/*-----------------------------------------*/
 	Retval = Sender.MsgHndlr(send_to_console , disp_msg);
 	/*-----------------------------------------*/
+	if((Retval != OK) || (doTerminate == TRUE) )
+	{
+		return PROCESS_TERMINATE;
+	}
+	else
+	{
+		return Retval;
+	}
 }
 
+
+int Fc_counter_elapsed(clock_t timestamp_previous)
+{
+	clock_t timestamp_current = clock();
+	if( Fc_time_difference(timestamp_previous , timestamp_current) >= 1000 )
+		return TRUE;
+	else
+		return FALSE;
+}/*end of Fc_counter_elapsed*/
+
+int Fc_time_difference(clock_t timestamp_previous , clock_t timestamp_current)
+{
+	if(timestamp_current > timestamp_previous)
+	{
+		return MILISECOND_VALUE(timestamp_current - timestamp_previous);
+	}
+	else
+	{
+		/*counter overflow condition*/
+		return MILISECOND_VALUE((MAX_CLOCK_CYCLE_TIME - timestamp_previous) + timestamp_current);
+	}
+}/*end of Fc_time_difference*/

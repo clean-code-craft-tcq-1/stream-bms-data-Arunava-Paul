1b59687d6dabe83a07f3353b2e398117c7edc14d
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-05-31 22:53:31 +0530

    Update Common_header.h

--- Sender_C/Common_header.h
+++ Sender_C/Common_header.h
@@ -14,4 +14,15 @@
 #define UPDATE 0
 #define ACCUMULATE 1
 
+#define TEST_CODE_ACTIVE  FALSE
+
+#if(TEST_CODE_ACTIVE == TRUE)
+int Test_OpHdlr_positive_trigger(void);
+int Test_OpHdlr_negetive_trigger(void);
+int send2console(const char *msg);
+int Test_BattParamColl(char *val , int col , int usage);
+int Test_CSV_HandlerNoFile(void);
+int Test_CSV_HandlerEndFile(void);
+#endif
+
 #endif

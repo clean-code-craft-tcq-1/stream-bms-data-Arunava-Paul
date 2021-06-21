bda4e8e1ab7fc8804c4292793b341f169a8794dd
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-06-21 21:46:12 +0530

    Update Receiver.c

--- Receiver_C/Receiver.c
+++ Receiver_C/Receiver.c
@@ -1,1 +1,65 @@
 
+#include "Receiver.h"
+
+/* Structure initialisation of battery parameters */
+struct BatteryParam_s BatteryParam[NUMOFPARAM] =
+{{"Battery_Temperature", TEMP_MIN , TEMP_MAX},
+{"Battery_SOC", CHRGRATE_MIN , CHRGRATE_MAX},
+{"Battery_Current",CURRENT_MIN, CURRENT_MAX},
+{"Battery_Charge_Rate",CURRENT_MIN, CURRENT_MAX}
+};
+
+float getParamValuefromConsoleCustom(char *scanLine, enum BATTERYPARAM batteryParam)
+{
+  char splitStr[NUMOFPARAM*4][20]={'\0'};
+  char * pch;
+  int k=0;
+  float temp;
+  char buffer[MAXLENGTH_INPUTSTRING];
+  
+  strcpy(buffer,scanLine);
+  pch = strtok(buffer," :\t");
+  
+  while (pch != NULL)
+  {
+    
+    strcpy((splitStr[k]),pch);
+      printf("\n%s",splitStr[k]); //debug
+    k++;
+    pch = strtok (NULL, " :\t");
+  }
+  
+  for(int j=0; j < (NUMOFPARAM*4);j=j+2)
+  {
+        
+      if(strcmp((splitStr[j]),BatteryParam[batteryParam].ParamName) == 0)
+      {
+          if(strcmp((splitStr[j+1]),"ERROR") == 0)
+         {
+          temp = ERROR_VALUE_FROM_RX;
+          }
+        else
+        {
+         temp = atof(splitStr[j+1]);
+          }
+         break;
+      }
+      else
+      {
+          temp = VALUE_NOTFOUND;
+      }
+  }
+
+  pch = NULL;
+
+  return temp;
+}
+
+int main(){
+//        char ip[1024] = "Battery_Temperature:54.5  Battery_SOC:0.4  Battery_Current:10  Battery_Charge_Rate:0.12";
+//   char *p;
+//    p = ip;
+//   float value = getParamValuefromConsoleCustom(p,CHARGERATE);
+//    printf("%f",value);
+    
+}

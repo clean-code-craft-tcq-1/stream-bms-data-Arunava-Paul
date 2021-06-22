912d9b194b47bb4cc0d6da31fe8a4e58a96c78ff
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-06-22 05:33:34 +0530

    Update Receiver.h

--- Receiver_C/Receiver.h
+++ Receiver_C/Receiver.h
@@ -1,47 +1,28 @@
-
-#include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
-//#ifdef _WIN32
-//#include <Windows.h>
-//#else
-//#include <unistd.h>
-//#endif
+typedef enum {
+  Failure,
+  Success
+} OperationMode;
 
-
-
-#define SOC_MIN      0.0
-#define SOC_MAX      20.0
-#define CHRGRATE_MIN 0.0
-#define CHRGRATE_MAX 50.0
-#define TEMP_MIN     20.0
-#define TEMP_MAX     80.0
-#define CURRENT_MIN  20.0
-#define CURRENT_MAX  80.0
-
-#define SMA_RANGE 5
-#define MAXLENGTH_INPUTSTRING 1024
-
-#define VALUE_NOTFOUND 65532
-#define ERROR_VALUE_FROM_RX 65535
-//define INVALID_VALUE 65535
-enum BATTERYPARAM {TEMPERATURE,SOC,CURRENT,CHARGERATE,NUMOFPARAM};
+#define Max_ArraySize 1024
+#define NumberOfAttributes 2
+extern float Temperature[Max_ArraySize];
+extern float SoC[Max_ArraySize];
+extern int NoOfEntery;
+extern float MinimumAttributeValueArray[NumberOfAttributes];
+extern float MaximumAttributeValueArray[NumberOfAttributes];
 
 
-struct BatteryParam_s
-{
-    char ParamName[20];
-    float minValue;
-    float maxValue;
-};
-
-struct BatteryParamOutput_s
-{
-    float SMA;
-    float minRxd;
-    float maxRxd;
-};
-
-extern struct BatteryParam_s BatteryParam[NUMOFPARAM];
+extern OperationMode BMS_ReadfromConsole();
+extern void Calculate_TemperatureMinandMaxRange(int NoOfEnteries);
+extern void Calculate_SoCMinandMaxRange(int NoOfEnteries);
+extern void Calculate_MaxParameterValue(float AttributeValue[Max_ArraySize], int NoOfEnteries, float *MaximumAttributeValue);
+extern void Calculate_MinParameterValue(float AttributeValue[Max_ArraySize], int NoOfEnteries, float *MinimumAttributeValue);
+extern void Calculate_TemperatureSimpleMovingAverage(int NoOfEnteries);
+extern void Calculate_SoCSimpleMovingAverage(int NoOfEnteries);
+extern float Calculate_SimpleMovingAverage(float AttributeValue[Max_ArraySize], int NoOfEnteries);
+extern OperationMode ReceiveDatafromSender();

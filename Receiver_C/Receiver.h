1b8ffd7b666579f0464daeba5d69854f56daba73
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-06-23 01:05:02 +0530

    Update Receiver.h

--- Receiver_C/Receiver.h
+++ Receiver_C/Receiver.h
@@ -1,28 +1,60 @@
-#include <math.h>
+/* *************************************************************************
+* File Name   : Receiver.h
+* Description : Header file for BMS Receiver type selection
+* Author      : Harshitha Subramani
+* ************************************************************************* */
+
+
+/* ******************************* Includes ******************************* */
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
-#include <time.h>
-
-typedef enum {
-  Failure,
-  Success
-} OperationMode;
-
-#define Max_ArraySize 1024
-#define NumberOfAttributes 2
-extern float Temperature[Max_ArraySize];
-extern float SoC[Max_ArraySize];
-extern int NoOfEntery;
-extern float MinimumAttributeValueArray[NumberOfAttributes];
-extern float MaximumAttributeValueArray[NumberOfAttributes];
+#include <unistd.h>
 
 
-extern OperationMode BMS_ReadfromConsole();
-extern void Calculate_TemperatureMinandMaxRange(int NoOfEnteries);
-extern void Calculate_SoCMinandMaxRange(int NoOfEnteries);
-extern void Calculate_MaxParameterValue(float AttributeValue[Max_ArraySize], int NoOfEnteries, float *MaximumAttributeValue);
-extern void Calculate_MinParameterValue(float AttributeValue[Max_ArraySize], int NoOfEnteries, float *MinimumAttributeValue);
-extern void Calculate_TemperatureSimpleMovingAverage(int NoOfEnteries);
-extern void Calculate_SoCSimpleMovingAverage(int NoOfEnteries);
-extern float Calculate_SimpleMovingAverage(float AttributeValue[Max_ArraySize], int NoOfEnteries);
-extern OperationMode ReceiveDatafromSender();
+/* ******************************* Defines ******************************* */
+
+
+
+#define CHRGRATE_MIN  -100
+#define CHRGRATE_MAX  100
+#define TEMP_MIN     -100
+#define TEMP_MAX      100
+#define SOC_MIN      -100
+#define SOC_MAX       100
+#define CURRENT_MIN  -100
+#define CURRENT_MAX   100
+
+#define SMA_RANGE     5
+#define BUFFER_SIZE  1024
+
+#define VALUE_NOTFOUND   65535
+#define INVALID_SMARANGE 65534
+#define INVALID_VALUE    0
+
+enum BATTERYPARAM {TEMPERATURE,SOC,CURRENT,CHARGERATE, NUMOFPARAM};
+
+
+struct BatteryParam_s
+{
+    char ParamName[200];
+    float minValue;
+    float maxValue;
+};
+
+struct BatteryParamOutput_s
+{
+    float MovingAvg;
+    float minRx;
+    float maxRx;
+};
+
+extern struct BatteryParam_s BatteryParam[NUMOFPARAM];
+
+/* ***************************** Prototypes ***************************** */
+float getParam_FromRxBuffer(char *ReadLine, enum BATTERYPARAM batteryParam);
+float MinimumOfTwoFloatNumbers(float value1, float value2);
+float MaximumOfTwoFloatNumbers(float value1, float value2);
+float movingAverageForRangeofValue(float *ptrArrNumbers, float *ptrSum, int param_id, int range, float nextNum);
+void RxCalculate_FromBMSParam(void);

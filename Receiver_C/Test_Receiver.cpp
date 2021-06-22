3089ee05b9c9ca1e9a22ae7a53983dd295923322
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-06-22 06:27:26 +0530

    Update Test_Receiver.cpp

--- Receiver_C/Test_Receiver.cpp
+++ Receiver_C/Test_Receiver.cpp
@@ -1,7 +1,7 @@
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "test/catch.hpp"
+#include "Receiver_C/test/catch.hpp"
 #include "bms_receiver.h"
 
 TEST_CASE("To check if file was successfully opened to read") 
